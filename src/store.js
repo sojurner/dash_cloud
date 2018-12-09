8c1d2b7bdb72b51084ef489a2e307c4d8aec5c7d
sojurner <pykim0591@gmail.com>
2018-12-08 19:16:27 -0800

    Add photos and todos to store

--- src/store.js
+++ src/store.js
@@ -1,10 +1,27 @@
-import Vue from "vue";
-import Vuex from "vuex";
+import Vue from 'vue';
+import Vuex from 'vuex';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {}
+  state: {
+    photos: [],
+    todos: []
+  },
+  mutations: {
+    setPhotos(state, photos) {
+      state.photos = [...state.photos, photos];
+    },
+    addTodo(state, todo) {
+      state.todos = [...state.todos, todo];
+    }
+  },
+  actions: {
+    setPhotos(context, photos) {
+      context.commit('setPhotos', photos);
+    },
+    addTodo(context, todo) {
+      context.commit('addTodo', todo);
+    }
+  }
 });
