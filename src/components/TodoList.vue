14f792f8b1d7f52fd320fd41f59271f38d228360
sojurner <pykim0591@gmail.com>
2018-12-10 20:29:51 -0700

    Modify todoList to render table for todos

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,5 +1,24 @@
 <template>
-  <Modal v-if="showModal"/>
+  <div class="todo-list">
+    <h1>
+      <i class="fas fa-clipboard-list"></i>
+      2dos
+    </h1>
+    <table>
+      <tbody v-if="todoList.length">
+        <tr class="todo-card">
+          <th @click="sortList(k)" v-for="k in keys">{{k}}</th>
+        </tr>
+        <tr v-for="todo in todoList">
+          <td>{{todo.title}}</td>
+          <td>{{todo.description}}</td>
+          <td>{{todo.importance}}</td>
+          <td>{{todo.category}}</td>
+          <td>{{todo.date}}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script>
