c6e0b067ea376fd3a25fd3dc616a07b97c5fadf3
sojurner <pykim0591@gmail.com>
2018-12-10 20:32:00 -0700

    Create LinkForm component

--- src/controllers/LinkForm.vue
+++ src/controllers/LinkForm.vue
@@ -1,8 +1,24 @@
 <template>
-  <form action>
-    <input autofocus ref="title" type="text" name="title" @keyup="handleChange" class="title">
-    <input type="text" name="body" @keyup="handleChange" class="link">
-    <button @click="handleClick" class="save-btn" :class="{'save-btn-active': clicked}"></button>
+  <form @submit="handleSubmit">
+    <h1 v-if="error">{{error}}</h1>
+    <h6 v-if="clicked">Link Saved!</h6>
+    <input
+      placeholder="Enter Title"
+      ref="title"
+      type="text"
+      name="title"
+      @keyup="handleChange"
+      class="title"
+    >
+    <input
+      placeholder="Enter Link"
+      ref="link"
+      type="url"
+      name="link"
+      @keyup="handleChange"
+      class="link"
+    >
+    <button class="save-btn" :class="{'save-btn-active': clicked}"></button>
   </form>
 </template>
 
@@ -14,38 +30,40 @@ export default {
   name: "forms",
   data() {
     return {
-      showModal: false,
       title: "",
-      body: "",
-      category: "",
-      importance: null,
+      Link: "",
       date: null,
       id: null,
-      clicked: false
+      clicked: false,
+      error: null
     };
   },
   methods: {
     handleChange(event) {
+      console.log("asdf");
       const { value, name } = event.target;
       this[name] = value;
     },
 
-    handleClick() {
-      const { title, body, category, importance, id } = this.$data;
-      if (title && body) {
-        this.clicked = true;
-        const todo = {
-          title,
-          body,
-          category,
-          id: Date.now(),
-          importance
-        };
-        this.$store.dispatch("addTodo", todo);
+    handleSubmit(event) {
+      event.preventDefault();
 
+      const { title, link, importance, id } = this;
+      if ((title, link)) {
+        this.clicked = true;
+        const linkObj = {
+          title,
+          link,
+          id: Date.now(),
+          date: moment().format("MM/DD/YYYY @ h:mm a")
+        };
+        this.$store.dispatch("addLink", linkObj);
         setTimeout(() => {
           this.$data.clicked = false;
-        }, 3000);
+          this.$root.$emit("closeModal");
+        }, 2000);
+      } else {
+        this.error = "Missing required Fields";
       }
     }
   }
@@ -54,14 +72,18 @@ export default {
 
 <style scoped>
 input {
-  height: 1.5rem;
-  width: 20em;
+  font-size: 1.3rem;
+  padding-left: 1rem;
+  letter-spacing: 2px;
+  height: 2rem;
+  width: 20rem;
   margin: 1rem;
   border-top: none;
   border-left: none;
   border-right: none;
+  border-bottom: 1px solid black;
   background-color: rgb(50, 68, 85);
-  color: aliceblue;
+  color: rgb(135, 167, 196);
 }
 
 input:focus {
