e0696bbdca1817e0d3dfb3007c531ab93834b881
sojurner <pykim0591@gmail.com>
2018-12-10 20:25:56 -0700

    Update route path

--- src/router.js
+++ src/router.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
+
 import Home from './views/Home.vue';
 
 Vue.use(Router);
@@ -12,13 +13,13 @@ export default new Router({
       component: Home
     },
     {
-      path: '/about',
-      name: 'about',
+      path: '/work',
+      name: 'work',
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () =>
-        import(/* webpackChunkName: "about" */ './views/About.vue')
+        import(/* webpackChunkName: "about" */ './views/Work.vue')
     }
   ]
 });
