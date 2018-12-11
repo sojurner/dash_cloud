9819ce4ed023e0ee69e9d998ae1f2ce269a5cd15
sojurner <pykim0591@gmail.com>
2018-12-10 20:29:15 -0700

    Add modal view component

---
+++ src/components/ModalView.vue
@@ -0,0 +1,36 @@
+<template>
+  <Modal v-if="showModal" :type="type"></Modal>
+</template>
+
+<script>
+import Modal from "./Modal.vue";
+
+export default {
+  name: "modal-view",
+  components: { Modal },
+  data() {
+    return {
+      showModal: false,
+      type: ""
+    };
+  },
+  computed: {
+    displayTodos() {}
+  },
+  mounted() {
+    this.$root.$on("openTodoModal", str => {
+      console.log(str);
+      this.showModal = true;
+      this.type = str;
+    });
+
+    this.$root.$on("closeModal", () => {
+      this.showModal = false;
+      this.todos = false;
+      this.links = false;
+    });
+
+    console.log(this.$store.state.todos);
+  }
+};
+</script>
