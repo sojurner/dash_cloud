996e570572a43c0ebeb651c935ecf85e05efbca1
sojurner <pykim0591@gmail.com>
2018-12-12 13:10:08 -0700

    Create Overview component; parent of weather and newsfeed

---
+++ src/views/Overview.vue
@@ -0,0 +1,24 @@
+<template>
+  <main>
+    <Newsfeed/>
+    <Weather/>
+  </main>
+</template>
+
+    <script>
+import Newsfeed from "@/components/Newsfeed.vue";
+import Weather from "@/components/Weather.vue";
+
+export default {
+  name: "overview",
+  components: {
+    Newsfeed,
+    Weather
+  }
+};
+</script>
+
+<style scoped>
+main {
+}
+</style>
