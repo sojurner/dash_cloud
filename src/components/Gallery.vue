da72f6f5d696050edaaad4bc78c60e2ebc80820d
sojurner <pykim0591@gmail.com>
2018-12-11 20:32:52 -0700

    Modify img rendering in template

--- src/components/Gallery.vue
+++ src/components/Gallery.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <div v-for="image in imageDisplay">
+  <div class="img-container">
+    <div class="img-wrapper" v-for="image in imageDisplay">
       <img :src="image" alt="unsplash images">
     </div>
   </div>
@@ -18,4 +18,27 @@ export default {
 </script>
 
 <style scoped>
+.img-container {
+  scroll-snap-type: mandatory;
+  scroll-snap-points-y: repeat(100vw);
+  scroll-snap-type: x mandatory;
+  overflow-x: auto;
+  display: flex;
+}
+
+.img-wrapper {
+  height: 86vh;
+  width: 100vw;
+  flex-direction: row;
+  flex-wrap: wrap;
+  scroll-snap-align: start;
+  min-width: 100vw;
+}
+img {
+  height: 100%;
+  width: 100%;
+  z-index: -1;
+  opacity: 0.5;
+  filter: grayscale(10%);
+}
 </style>
