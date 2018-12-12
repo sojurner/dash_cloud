7ba709ff8a3189645c48e698dbdcca911265353a
sojurner <pykim0591@gmail.com>
2018-12-11 20:35:17 -0700

    Move fetch calls into methods and call methods in mounted

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,25 +1,52 @@
 <template>
   <div class="home">
-    <Dashboard/>
+    <Newsfeed/>
     <Gallery/>
+    <Weather/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import Dashboard from "@/components/Dashboard.vue";
+import Newsfeed from "@/components/Newsfeed.vue";
 import Gallery from "@/components/Gallery.vue";
-import { fetchRandomPhotos } from "../utils/apiCalls";
+import Weather from "@/components/Weather.vue";
+import * as call from "../utils/apiCalls";
 
 export default {
   name: "home",
   components: {
-    Dashboard,
-    Gallery
+    Newsfeed,
+    Gallery,
+    Weather
   },
-  async mounted() {
-    let photos = await fetchRandomPhotos();
-    this.$store.dispatch("setPhotos", photos);
+  methods: {
+    getWeather() {
+      if (!this.$store.state.weather) {
+        const x = navigator.geolocation.getCurrentPosition(async location => {
+          const { latitude, longitude } = location.coords;
+          let weather = await call.fetchWeather(latitude, longitude);
+          this.$store.dispatch("setWeather", weather);
+        });
+      }
+    },
+    async getPhotos() {
+      if (!this.$store.state.photos.length) {
+        let photos = await call.fetchRandomPhotos();
+        this.$store.dispatch("setPhotos", photos);
+      }
+    },
+    async getNews() {
+      if (!this.$store.state.news) {
+        let news = await call.fetchNews();
+        this.$store.dispatch("setNews", news);
+      }
+    }
+  },
+  mounted() {
+    this.getWeather();
+    this.getPhotos();
+    this.getNews();
   }
 };
 </script>
